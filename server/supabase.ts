afc338cd8ac2e56cf2da5667258a9a2f92976948
Andrew Rowley <andrew@internetdrew.com>
2025-11-11 16:01:58 -0500

    add supabase server client

---
+++ server/supabase.ts
@@ -0,0 +1,50 @@
+import { createServerClient, serializeCookieHeader } from "@supabase/ssr";
+import { createClient } from "@supabase/supabase-js";
+import { Request, Response } from "express";
+import type { Database } from "../shared/database.types";
+import dotenv from "dotenv";
+import path from "path";
+
+const baseDir: string =
+  typeof __dirname !== "undefined" ? __dirname : process.cwd();
+
+dotenv.config({ path: path.resolve(baseDir, "../.env") });
+
+const supabaseUrl = process.env.VITE_SUPABASE_URL;
+const supabaseKey = process.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
+
+export const createServerSupabaseClient = (req: Request, res: Response) => {
+  return createServerClient<Database>(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll() {
+        return Object.keys(req.cookies).map((name) => ({
+          name,
+          value: req.cookies[name] || "",
+        }));
+      },
+      setAll(cookiesToSet) {
+        cookiesToSet.forEach(({ name, value, options }) =>
+          res.appendHeader(
+            "Set-Cookie",
+            serializeCookieHeader(name, value, options),
+          ),
+        );
+      },
+    },
+  });
+};
+
+const supabaseAdminKey = process.env.SUPABASE_SECRET_KEY;
+
+if (!supabaseAdminKey) {
+  throw new Error("Missing Supabase admin key");
+}
+
+export const supabaseAdminClient = createClient<Database>(
+  supabaseUrl,
+  supabaseAdminKey,
+);
