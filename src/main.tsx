88242743eb4462dd52ee7dcd1bf1d476e1c87c34
Andrew Rowley <andrew@internetdrew.com>
2025-11-11 15:51:33 -0500

    add auth and protected route

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,42 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router';
-import { queryClient } from '../utils/trpc';
-import { QueryClientProvider } from '@tanstack/react-query';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { queryClient } from "../utils/trpc";
+import { QueryClientProvider } from "@tanstack/react-query";
+import Login from "./routes/Login.tsx";
+import { AuthProvider } from "./contexts/auth.tsx";
+import ProtectedRoute from "./components/ProtectedRoute.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/",
     element: (
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
+      <ProtectedRoute>
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
+      </ProtectedRoute>
     ),
+    children: [
+      {
+        path: "/",
+        index: true,
+        element: <h1>hello</h1>,
+      },
+    ],
   },
 ]);
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>,
 );
