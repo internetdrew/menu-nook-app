7f192944ac9f7a37297870a33bddb9c2eb53d712
Andrew Rowley <andrew@internetdrew.com>
2025-11-11 16:38:55 -0500

    remove trpc call

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
 import { Outlet } from "react-router";
-import { trpc } from "../utils/trpc";
-import { useQuery } from "@tanstack/react-query";
 
 function App() {
-  const { data, isLoading } = useQuery(
-    trpc.greeting.queryOptions({ intro: "Welcome to" }),
-  );
-
   return (
     <div>
       <Outlet />
