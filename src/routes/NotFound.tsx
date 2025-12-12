9f52d0eed26a622ae323b4d47501f18e5f302ead
Andrew Rowley <andrew@internetdrew.com>
2025-12-12 14:07:18 -0500

    add props to not found component

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,20 +1,25 @@
 import { Badge } from "@/components/ui/badge";
 import { Link } from "react-router";
 
-const NotFound = () => {
+interface NotFoundProps {
+  title: string;
+  message: string;
+  href: string;
+  hrefText: string;
+}
+
+export const NotFound = ({ title, message, href, hrefText }: NotFoundProps) => {
   return (
     <div className="p-8 text-center">
       <Badge className="mt-36 mb-4 bg-red-100 text-red-800">404</Badge>
-      <h1 className="text-2xl font-bold">Page Not Found</h1>
-      <p>The page you're looking for does not exist.</p>
+      <h1 className="text-2xl font-bold">{title}</h1>
+      <p>{message}</p>
       <Link
-        to="/"
+        to={href}
         className="mt-4 inline-block text-pink-600 underline-offset-4 hover:underline"
       >
-        Go back to Home
+        {hrefText}
       </Link>
     </div>
   );
 };
-
-export default NotFound;
