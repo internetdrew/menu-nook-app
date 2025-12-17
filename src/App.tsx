835cb80c8b48d536dc9847097eb386a1da34d380
Andrew Rowley <andrew@internetdrew.com>
2025-12-17 10:41:07 -0500

    fix spacing

--- src/App.tsx
+++ src/App.tsx
@@ -35,9 +35,9 @@ function App() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 pr-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-          <div className="flex flex-1 items-center gap-2 px-4">
-            <SidebarTrigger className="-ml-1" />
+        <header className="bg-background sticky top-0 flex h-16 shrink-0 items-center gap-2 px-4 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+          <div className="flex flex-1 items-center gap-2">
+            <SidebarTrigger />
             {activePlace && (
               <>
                 <Separator
