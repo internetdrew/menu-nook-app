1a4b56a1bfb1c3cde74a65a00671e474f77061da
Andrew Rowley <andrew@internetdrew.com>
2025-12-17 11:57:19 -0500

    add initial business router

---
+++ server/routers/businessRouter.ts
@@ -0,0 +1,56 @@
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { protectedProcedure, router } from "../trpc";
+import { supabaseAdminClient } from "../supabase";
+
+export const businessRouter = router({
+  create: protectedProcedure
+    .input(
+      z.object({
+        name: z.string().min(1),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      const { data: place, error } = await supabaseAdminClient
+        .from("businesses")
+        .insert({
+          name: input.name,
+          user_id: ctx.user.id,
+        })
+        .select()
+        .single();
+
+      if (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
+
+      return place;
+    }),
+
+  delete: protectedProcedure
+    .input(
+      z.object({
+        businessId: z.uuid(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { data, error } = await supabaseAdminClient
+        .from("businesses")
+        .delete()
+        .eq("id", input.businessId)
+        .select()
+        .single();
+
+      if (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
+
+      return data;
+    }),
+});
