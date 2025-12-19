80cc21ee80dded8c079fc134b72acd437b5a8c55
Andrew Rowley <andrew@internetdrew.com>
2025-12-19 12:53:01 -0500

    fix dialog trigger

--- src/components/MenuSwitcher.tsx
+++ src/components/MenuSwitcher.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ChevronsUpDown, MapPin, Plus } from "lucide-react";
+import { ChevronsUpDown, FolderTree, Plus } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -29,7 +29,7 @@ export function MenuSwitcher() {
 
   const triggerDialog = () => {
     setRenderDropdown(false);
-    navigate("/");
+    setIsDialogOpen(true);
   };
 
   return (
@@ -42,7 +42,7 @@ export function MenuSwitcher() {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <MapPin className="size-4" />
+                <FolderTree className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">
