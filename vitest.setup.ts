6cc7ccd8c52bb8d569c7eef4d9a98592180848f9
Andrew Rowley <andrew@internetdrew.com>
2025-12-29 13:41:51 -0500

    add match media for shad in test

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -2,6 +2,19 @@ import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./src/mocks/node.ts";
 import "@testing-library/jest-dom/vitest";
 
+// Mock window.matchMedia for tests
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => true,
+  }),
+});
+
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
