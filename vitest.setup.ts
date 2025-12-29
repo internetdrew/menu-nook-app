0de08943fb9a21b04e4e88f1c407f74233d9b559
Andrew Rowley <andrew@internetdrew.com>
2025-12-29 08:33:12 -0500

    add testing setup to config

---
+++ vitest.setup.ts
@@ -0,0 +1,7 @@
+import { beforeAll, afterEach, afterAll } from "vitest";
+import { server } from "./src/mocks/node.ts";
+import "@testing-library/jest-dom/vitest";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
