263fc6e1571c8620a00c4584f286808b2014bd61
Andrew Rowley <andrew@internetdrew.com>
2025-12-29 08:30:33 -0500

    add test utils

---
+++ src/utils/test/renderApp.tsx
@@ -0,0 +1,26 @@
+// test/utils/renderApp.tsx
+import { render } from "@testing-library/react";
+import { createMemoryRouter, RouterProvider } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { AuthProvider } from "@/contexts/auth";
+import { MenuProvider } from "@/contexts/ActiveMenuContext";
+import { routes } from "@/routes";
+import { createTestQueryClient } from "./createTestQueryClient";
+
+export function renderApp(initialEntries: string[] = ["/"]) {
+  const router = createMemoryRouter(routes, {
+    initialEntries,
+  });
+
+  const queryClient = createTestQueryClient();
+
+  return render(
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <MenuProvider>
+          <RouterProvider router={router} />
+        </MenuProvider>
+      </QueryClientProvider>
+    </AuthProvider>,
+  );
+}
