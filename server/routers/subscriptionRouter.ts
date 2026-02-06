aad05de8106b6041fa6af9a10867c1600b2dacfa
Andrew Rowley <andrew@internetdrew.com>
2026-02-06 11:57:08 -0500

    update for business id based fetch

--- server/routers/subscriptionRouter.ts
+++ server/routers/subscriptionRouter.ts
@@ -1,21 +1,24 @@
+import { z } from "zod";
 import { TRPCError } from "@trpc/server";
-import { protectedProcedure, router } from "../trpc";
+import { publicProcedure, router } from "../trpc";
 
 export const subscriptionRouter = router({
-  getForUser: protectedProcedure.query(async ({ ctx }) => {
-    const { data: subscription, error } = await ctx.supabase
-      .from("subscriptions")
-      .select("*")
-      .eq("user_id", ctx.user.id)
-      .maybeSingle();
+  getForBusiness: publicProcedure
+    .input(z.object({ businessId: z.uuid() }))
+    .query(async ({ ctx, input }) => {
+      const { data: subscription, error } = await ctx.supabase
+        .from("subscriptions")
+        .select("*")
+        .eq("business_id", input.businessId)
+        .maybeSingle();
 
-    if (error) {
-      throw new TRPCError({
-        code: "INTERNAL_SERVER_ERROR",
-        message: error.message,
-      });
-    }
+      if (error) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: error.message,
+        });
+      }
 
-    return subscription;
-  }),
+      return subscription;
+    }),
 });
