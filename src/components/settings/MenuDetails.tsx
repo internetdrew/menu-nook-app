4b5d5a4c515e7a54d5c55b0f23d8341be5d1b176
Andrew Rowley <andrew@internetdrew.com>
2026-03-29 18:25:53 -0400

    add skeleton to menu details

--- src/components/settings/MenuDetails.tsx
+++ src/components/settings/MenuDetails.tsx
@@ -1,13 +1,5 @@
 import { queryClient, trpc } from "@/utils/trpc";
 import { useMutation } from "@tanstack/react-query";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
@@ -15,11 +7,11 @@ import { toast } from "sonner";
 import * as z from "zod";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
 import { useEffect, useState } from "react";
 import DeleteMenuAlertDialog from "@/components/dialogs/DeleteMenuAlertDialog";
 import { useMenuContext } from "@/contexts/ActiveMenuContext";
 import { useNavigate } from "react-router";
+import { Skeleton } from "../ui/skeleton";
 
 const menuFormSchema = z.object({
   name: z
@@ -30,7 +22,7 @@ const menuFormSchema = z.object({
 
 export const MenuDetails = () => {
   const [deleteMenuDialogOpen, setDeleteMenuDialogOpen] = useState(false);
-  const { activeMenu, menus, setActiveMenu } = useMenuContext();
+  const { activeMenu, menus, setActiveMenu, loading } = useMenuContext();
   const navigate = useNavigate();
 
   const menuForm = useForm<z.infer<typeof menuFormSchema>>({
@@ -80,70 +72,64 @@ export const MenuDetails = () => {
     navigate("/");
   };
 
+  if (loading)
+    return (
+      <div>
+        <Skeleton className="h-10 w-1/2" />
+        <Skeleton className="mt-8 mb-1 h-5 w-1/4" />
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="mt-2 ml-auto h-10 w-1/8" />
+      </div>
+    );
+
   return (
-    <Card>
-      <CardHeader>
-        <CardAction>
+    <div>
+      <h2 className="font-medium">Menu Settings</h2>
+      <p className="text-muted-foreground mb-6 text-sm">
+        Update your menu name or delete your menu and all of its categories and
+        items.
+      </p>
+      <form id="menu-name" onSubmit={menuForm.handleSubmit(onMenuSubmit)}>
+        <Controller
+          name="name"
+          control={menuForm.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="menu-name">Menu Name</FieldLabel>
+              <Input
+                {...field}
+                id="menu-name"
+                aria-invalid={fieldState.invalid}
+                placeholder="Eg. Lunch Menu"
+                autoComplete="off"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <div className="mt-4 flex w-full items-center justify-between gap-2">
           <DeleteMenuAlertDialog
             menu={activeMenu}
             open={deleteMenuDialogOpen}
             onOpenChange={setDeleteMenuDialogOpen}
             onDeleted={handleMenuDeleted}
           />
-        </CardAction>
-        <CardTitle>Menu Details</CardTitle>
-        <CardDescription>
-          Manage settings for{" "}
-          <span className="font-semibold">{activeMenu?.name}</span>.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form
-          id="menu-name"
-          className="grid grid-cols-[1fr_auto] items-end gap-2"
-          onSubmit={menuForm.handleSubmit(onMenuSubmit)}
-        >
-          <Controller
-            name="name"
-            control={menuForm.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor="menu-name">Menu Name</FieldLabel>
-                <Input
-                  {...field}
-                  id="menu-name"
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Eg. Lunch Menu"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
-          <div
-            className={cn(
-              "grid transition-all duration-150 ease-in-out",
-              menuForm.formState.isDirty
-                ? "grid-cols-[1fr] opacity-100"
-                : "grid-cols-[0fr] opacity-0",
-            )}
+          <Button
+            type="submit"
+            form="menu-name"
+            size={"sm"}
+            className="overflow-hidden"
+            disabled={
+              menuForm.formState.isSubmitting || !menuForm.formState.isDirty
+            }
+            tabIndex={menuForm.formState.isDirty ? 0 : -1}
+            aria-hidden={!menuForm.formState.isDirty}
           >
-            <Button
-              type="submit"
-              form="menu-name"
-              size={"sm"}
-              className="overflow-hidden"
-              disabled={menuForm.formState.isSubmitting}
-              tabIndex={menuForm.formState.isDirty ? 0 : -1}
-              aria-hidden={!menuForm.formState.isDirty}
-            >
-              {menuForm.formState.isSubmitting ? "Saving..." : "Save"}
-            </Button>
-          </div>
-        </form>
-      </CardContent>
-    </Card>
+            {menuForm.formState.isSubmitting ? "Saving..." : "Save"}
+          </Button>
+        </div>
+      </form>
+    </div>
   );
 };
