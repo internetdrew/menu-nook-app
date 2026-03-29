1ad6e094e0ce14d8297760ad39a91c6d9d729044
Andrew Rowley <andrew@internetdrew.com>
2026-03-29 18:15:37 -0400

    add skeleton to business details

--- src/components/settings/BusinessDetails.tsx
+++ src/components/settings/BusinessDetails.tsx
@@ -1,12 +1,5 @@
 import { queryClient, trpc } from "@/utils/trpc";
 import { useMutation, useQuery } from "@tanstack/react-query";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Controller, useForm } from "react-hook-form";
@@ -14,9 +7,9 @@ import { toast } from "sonner";
 import * as z from "zod";
 import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
 import { useAuth } from "@/contexts/auth";
 import { useEffect } from "react";
+import { Skeleton } from "../ui/skeleton";
 
 const businessFormSchema = z.object({
   name: z
@@ -28,7 +21,7 @@ const businessFormSchema = z.object({
 export const BusinessDetails = () => {
   const { user, isLoading: authLoading } = useAuth();
 
-  const { data: business } = useQuery(
+  const { data: business, isLoading: businessLoading } = useQuery(
     trpc.business.getForUser.queryOptions(undefined, {
       enabled: !!user && !authLoading,
     }),
@@ -73,61 +66,59 @@ export const BusinessDetails = () => {
     );
   }
 
+  if (businessLoading)
+    return (
+      <div>
+        <Skeleton className="h-10 w-1/2" />
+        <Skeleton className="mt-8 mb-1 h-5 w-1/4" />
+        <Skeleton className="h-10 w-full" />
+        <Skeleton className="mt-2 ml-auto h-10 w-1/8" />
+      </div>
+    );
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Business Details</CardTitle>
-        <CardDescription>
-          If it involves your business, it can be updated here.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form
-          id="business-name"
-          className="grid grid-cols-[1fr_auto] items-end gap-2"
-          onSubmit={businessForm.handleSubmit(onBusinessSubmit)}
+    <div>
+      <h2 className="font-medium">Business Settings</h2>
+      <p className="text-muted-foreground mb-6 text-sm">
+        Basic information about your business.
+      </p>
+      <form
+        id="business-name"
+        onSubmit={businessForm.handleSubmit(onBusinessSubmit)}
+      >
+        <Controller
+          name="name"
+          control={businessForm.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor="business-name">Business Name</FieldLabel>
+              <Input
+                {...field}
+                id="business-name"
+                aria-invalid={fieldState.invalid}
+                placeholder="Eg. The Blonde Wolf"
+                autoComplete="off"
+              />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+      </form>
+      <div className="mt-4 flex justify-end">
+        <Button
+          type="submit"
+          form="business-name"
+          size={"sm"}
+          disabled={
+            businessForm.formState.isSubmitting ||
+            !businessForm.formState.isDirty
+          }
+          tabIndex={businessForm.formState.isDirty ? 0 : -1}
+          aria-hidden={!businessForm.formState.isDirty}
         >
-          <Controller
-            name="name"
-            control={businessForm.control}
-            render={({ field, fieldState }) => (
-              <Field data-invalid={fieldState.invalid}>
-                <FieldLabel htmlFor="business-name">Business Name</FieldLabel>
-                <Input
-                  {...field}
-                  id="business-name"
-                  aria-invalid={fieldState.invalid}
-                  placeholder="Eg. The Blonde Wolf"
-                  autoComplete="off"
-                />
-                {fieldState.invalid && (
-                  <FieldError errors={[fieldState.error]} />
-                )}
-              </Field>
-            )}
-          />
-          <div
-            className={cn(
-              "grid transition-all duration-150 ease-in-out",
-              businessForm.formState.isDirty
-                ? "grid-cols-[1fr] opacity-100"
-                : "grid-cols-[0fr] opacity-0",
-            )}
-          >
-            <Button
-              type="submit"
-              form="business-name"
-              size={"sm"}
-              className="overflow-hidden"
-              disabled={businessForm.formState.isSubmitting}
-              tabIndex={businessForm.formState.isDirty ? 0 : -1}
-              aria-hidden={!businessForm.formState.isDirty}
-            >
-              {businessForm.formState.isSubmitting ? "Saving..." : "Save"}
-            </Button>
-          </div>
-        </form>
-      </CardContent>
-    </Card>
+          {businessForm.formState.isSubmitting ? "Saving..." : "Save"}
+        </Button>
+      </div>
+    </div>
   );
 };
