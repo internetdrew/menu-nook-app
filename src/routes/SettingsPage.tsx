be1bf12da04990de5adc65e8aa18988b6f6c988f
Andrew Rowley <andrew@internetdrew.com>
2026-03-29 20:16:59 -0400

    conditionally render menu settings

--- src/routes/SettingsPage.tsx
+++ src/routes/SettingsPage.tsx
@@ -1,8 +1,11 @@
 import { BusinessDetails } from "@/components/settings/BusinessDetails";
 import { MenuDetails } from "@/components/settings/MenuDetails";
 import { Separator } from "@/components/ui/separator";
+import { useMenuContext } from "@/contexts/ActiveMenuContext";
 
 export const SettingsPage = () => {
+  const { activeMenu } = useMenuContext();
+
   return (
     <div className="mx-auto mt-12 max-w-lg px-2">
       <header className="flex items-center">
@@ -11,8 +14,12 @@ export const SettingsPage = () => {
 
       <section className="mt-6 space-y-6">
         <BusinessDetails />
-        <Separator />
-        <MenuDetails />
+        {activeMenu && (
+          <>
+            <Separator />
+            <MenuDetails />
+          </>
+        )}
       </section>
     </div>
   );
