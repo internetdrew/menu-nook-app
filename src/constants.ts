b1c0cd0e405b7dff94d89ca3613537738ffe2d73
Andrew Rowley <andrew@internetdrew.com>
2026-05-19 10:53:33 -0400

    add new constants

--- src/constants.ts
+++ src/constants.ts
@@ -6,3 +6,7 @@ export const linkClasses =
   "text-pink-600 underline-offset-4 hover:text-pink-800 underline";
 
 export const MAX_MENUS_PER_BUSINESS = 10;
+export const accordionEaseOut = [0.215, 0.61, 0.355, 1] as const;
+
+export const sortableTransition =
+  "transform 250ms cubic-bezier(0.25, 1, 0.5, 1), opacity 180ms ease-out";
